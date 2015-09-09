ee3b13091e18f0f4ce0124fb7374938de326cfdc
GiveMyIDBack <453429978@qq.com>
2015-09-09 23:13:16 +0800

    Revert "1212"
    This reverts commit 5686cfc6d8b6bef9700a5e67b4959794d8f23623.

--- AirSystem/src/DBTools2/DBTool.java
+++ AirSystem/src/DBTools2/DBTool.java
@@ -90,8 +90,8 @@ public class DBTool {
 	        	
 	        	sql = "select * from airline  "
 	        	+"where airline.date = ?  "
-	    		+"and airline.origin = ?  "
-	    		+"and airline.destination = ?  ";
+	    		+"and airline.origin =?  "
+	    		+"and airline.destination =?  ";
 	        	/*
 	        	String sql = "select * from airline  "+
 	        			"where airline.date = ?  "
@@ -225,44 +225,69 @@ public class DBTool {
 
 	            try {
 	                if (ps!=null) ps.close();
-	                //if(conn!=null) conn.close();
+	                if(conn!=null) conn.close();
 	            } catch (SQLException e) {
 	                e.printStackTrace();
 	            }
 	        }
 	    }
 	    
-	    //之前要查aid,cid,did,sid是否是有效的，aid的该seat_type是否还有座位，该seatid是否没人要
-	    public void bookTicket(String cid,String aid,String seat_type,String seatid,String sid,String did){
+	    /*
+	    public void Book(String aid,String seat_type,int cid,int tid,int did){
 	        try {
 	        	
-	        	//生成订单号
-	        	String orderid = java.util.UUID.randomUUID().toString().replaceAll("-","").toUpperCase(); 
-	        	System.out.println(orderid);
-	        	String sql="";
+	        	String sql = "select * from airline  "
+	        	+"where airline.date = ?  "
+	    		+"and airline.origin =?  "
+	    		+"and airline.destination =?  ";
+	        	String sql="insert (aid,)"
+	        	*/
 	        	
-	        	sql=" INSERT INTO orders VALUES ( ? , ?  ,?  ,? ,?  ,?  ,?  )  ";
-	        	
+	        	/*
+	        	String sql = "select * from airline  "+
+	        			"where airline.date = ?  "
+	    				+"and airline.time =?  "
+	    		+"and airline.origin =?  "
+	    				+"and airline.destination =?  ";
+	    				*/
+	    /*
 	        	ps = conn.prepareStatement(sql);
-	            ps.setString(1, orderid);
-	            ps.setString(2, cid);
-	            ps.setString(3, aid);
-	            ps.setString(4,seat_type);
-	            ps.setString(5,seatid );
-	            ps.setString(6,sid);
-	            ps.setString(7,sid);
-
-	
-	            int rs=ps.executeUpdate();  
+	            ps.setInt(1, CurrentDate);
+	            ps.setString(2, CurrentOrigin);
+	            ps.setString(3, CurrentDestination);
 	            
-	            if(rs!=0)
-	            	System.out.println("成功插入");
-	            else
-	            	System.out.println("错误，请重试");
-	            	
-	        	
+	            ResultSet rs = ps.executeQuery();  
 	            
-	            } catch (SQLException e) {
+	            String aid="";
+	            String origin = "";
+	            String destination = "";
+	            int date=0;
+	            int time=0;
+	            int first_num=0;
+	            int business_num=0;
+	            int economic_num=0;
+	            String type="";
+	            
+				int id = 0;
+				while (rs.next()) {
+					aid=rs.getString("aid");
+					origin = rs.getString("origin");
+					destination = rs.getString("destination");
+					//name = new String(name.getBytes("ISO-8859-1"), "GB2312");
+					date=rs.getInt("date");
+					time=rs.getInt("time");
+					first_num=rs.getInt("first_num");
+					business_num=rs.getInt("business_num");
+					economic_num=rs.getInt("economic_num");
+					type = rs.getString("type");
+					System.out.println(aid+"  "+origin+"  "
+							+destination+"  "+date+"  "+time+"  "+
+							first_num+"  "+business_num+"  "+economic_num+"  "+type
+							);
+				}
+				rs.close();           
+	            
+	        } catch (SQLException e) {
 	            e.printStackTrace();
 	        } catch (Exception e) {
 					e.printStackTrace();
@@ -270,62 +295,17 @@ public class DBTool {
 
 	            try {
 	                if (ps!=null) ps.close();
-	                //if(conn!=null) conn.close();
+	                if(conn!=null) conn.close();
 	            } catch (SQLException e) {
 	                e.printStackTrace();
 	            }
 	        }
 	    }
-	    public void cancelTicket(String CurrentOrderid){
-	        try {
-	        	
-	        	
-	        	String sql="";
-	        	
-	        	sql=" DELETE FROM orders WHERE orderid =?  ";
-	        	
-	        	
-	        	ps = conn.prepareStatement(sql);
-	            ps.setString(1, CurrentOrderid);
-	            
-
-	
-	            int rs=ps.executeUpdate();  
-	            
-	            if(rs!=0)
-	            	System.out.println("成功删除订单");
-	            else
-	            	System.out.println("错误，请重试");
-	            	
-	        	
-	            
-	            } catch (SQLException e) {
-	            e.printStackTrace();
-	        } catch (Exception e) {
-					e.printStackTrace();
-			}finally{
-
-	            try {
-	                if (ps!=null) ps.close();
-	                //if(conn!=null) conn.close();
-	            } catch (SQLException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
-	    
+	    */
 	    public static void main(String[] args) {
 	        DBTool myhandler = new DBTool();
 	        myhandler.getConn("root","127127");
-	       //myhandler.getAirline(20100908,"北京","上海");
-	        myhandler.bookTicket("003","MU375","ECONOMIC","17A","002","001");
-	        myhandler.cancelTicket("0DDD961DB8F4443585816A8594A4FE8C");
-	        try {
-               if(myhandler.conn!=null) myhandler.conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-	       
+	        myhandler.getAirline(20100908,"北京","上海");
 	        
 	        
 	    }
